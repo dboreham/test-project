e56f17ea6b954400615e61687566aaced7f1020a
David Boreham <david@bozemanpass.com>
2023-04-20 16:01:59 -0600

    Add container build

--- scripts/run-test.sh
+++ scripts/run-test.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd ${SCRIPT_DIR}
-docker compose -f ../compose/docker-compose.yml up -d
+docker build -t dboreham/test-container:test -f ../container/Dockerfile ../container
+echo "Images in the registry:"
+docker image ls
+echo "Running the container I built:"
+docker run dboreham/test-container:test
+docker compose --verbose --project-directory ../compose -f ../compose/docker-compose.yml up -d
 docker compose -f ../compose/docker-compose.yml exec test ls -l /
 docker compose -f ../compose/docker-compose.yml exec test cat /config-file.txt
 docker compose -f ../compose/docker-compose.yml exec test cat /test-file.txt
